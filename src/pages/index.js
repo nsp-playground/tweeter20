31ca73137b6637d9ad518fdd6f7ca6e461046417
Nazmuz Pranto <npranto4@gmail.com>
2023-03-18 04:18:27 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Bug 3</h1>
+        <h1>Bug 4</h1>
         <section className="section top">
           <Profile username="shakib" status="Offline" />
         </section>
