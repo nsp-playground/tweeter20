a91f0611191e7df7d9b04f249159dddaee2cd4e8
Nazmuz Pranto <npranto@gmail.com>
2023-03-25 00:28:39 -0400

    adds feat/12345 - 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Test: Deploy I</h1>
+        <h1>Test: Feat/12345</h1>
         <section className="section top">
           <Profile username="shakib" status="Offline" />
         </section>
