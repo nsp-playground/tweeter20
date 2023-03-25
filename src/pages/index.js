66bac91efd75346ee8227e391a66897a6ce673b6
Nazmuz Pranto <npranto@gmail.com>
2023-03-25 13:28:55 -0400

    clean slate

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Test: Deploy I</h1>
         <section className="section top">
           <Profile username="shakib" status="Offline" />
         </section>
