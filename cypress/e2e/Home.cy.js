9184513c6c84d30a4a33271c1d428bc1018e3ddc
npranto <npranto@gmail.com>
2023-04-15 04:27:03 -0400

    feat: #62 - adds home page based on wireframe

--- cypress/e2e/Home.cy.js
+++ cypress/e2e/Home.cy.js
@@ -1,6 +1,9 @@
 describe("Home", () => {
   it("should render `Home` page", () => {
     cy.visit("http://localhost:3000/");
-    cy.get("h1").contains("Welcome to Tweeter 20");
+    cy.get("h1").contains("All Community Activities in Your Hands");
+    cy.get("p").contains(
+      "Create and promote their own events, connect with other community members, and share their experiences with others"
+    );
   });
 });
